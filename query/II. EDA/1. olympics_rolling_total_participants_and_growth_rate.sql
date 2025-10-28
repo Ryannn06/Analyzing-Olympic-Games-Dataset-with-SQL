e0101dfc695f6284d325438dfe035fd1571c5f35
Ryannn06 <ryanangelo.delacruz12@gmail.com>
2025-10-28 22:39:36 +0800

    add outputs

--- query/II. EDA/1. olympics_rolling_total_participants_and_growth_rate.sql
+++ query/II. EDA/1. olympics_rolling_total_participants_and_growth_rate.sql
@@ -10,15 +10,19 @@ WITH athlete_region AS (
         COUNT(DISTINCT region) AS total_participants
     FROM athlete_region
     GROUP BY games, season
-    ORDER BY games
 ) 
 SELECT 
     games,
     season,
     total_participants,
     SUM(total_participants) OVER(PARTITION BY season ORDER BY games ASC 
-                                ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS rolling_total_participants
-FROM participant_cte;
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS rolling_total_participants,
+    COALESCE(ROUND(
+        ((total_participants::numeric - LAG(total_participants) OVER(PARTITION BY season ORDER BY games)::numeric)
+        / NULLIF(LAG(total_participants) OVER(PARTITION BY season ORDER BY games)::numeric,0))*100, 2
+    ), 0) AS growth_rate_in_pct                      
+FROM participant_cte
+ORDER BY games;
 
--- Summer olympics had more participants 
+-- Summer olympics had signifincantly more participants 
 -- as compared with Winter olympics over the years
